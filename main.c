fd48dd6cc8e27e293e4c434d0b757b74c9b0e50b
SalomeMathe <salome@DESKTOP-B8RAIOP.>
2025-04-01 10:58:19 +0200

    ceci a ete edit

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 void main() {
-  printf("Hello a tous les amis!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  printf("Heok boomerrrrrrrrrrrrrrrrrrrrrllo a tous les amis!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 }
 
